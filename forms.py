6cfb7f6841a1ca0734b7b2c68d59e638ba2b2834
Rory Hughes <rory.hughes.rory@gmail.com>
2019-02-08 22:22:35 +0000

    Refactors files

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class Search(FlaskForm):
-    search_text = StringField('Input a word, words or hastags', validators=[DataRequired()])
-    submit = SubmitField('Search')
