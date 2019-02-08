83a69f73a0a77f1debd019ff167c22e3df714494
Rory Hughes <rory.hughes.rory@gmail.com>
2019-02-08 21:16:24 +0000

    Adds new fields to FTSEntry

--- app/db_builder.py
+++ app/db_builder.py
@@ -9,12 +9,30 @@ def create_db(path_to_db):
 
     db= SqliteExtDatabase(path_to_db,pragmas=pragmas)
     print('SQL database built at ' + path_to_db)
-    
+
     class FTSEntry(FTSModel):
         sentiment = IntegerField()
         username = CharField()
         date = DateTimeField()
         content = SearchField()
+
+        class Meta:
+            database = db
+
+    return FTSEntry
+
+def create_large_db(path_to_db):
+
+    db= SqliteExtDatabase(path_to_db,pragmas=pragmas)
+    print('SQL database built at ' + path_to_db)
+
+    class FTSEntry(FTSModel):
+        sentiment = FloatField()
+        user_id = CharField()
+        created_at = DateTimeField()
+        text = SearchField() 
+        hashtags = CharField()
+        user_mentions = CharField()
 
         class Meta:
             database = db
