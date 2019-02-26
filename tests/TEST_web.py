1d3b5551c42cd4cdc2ca524989d0f7e86c7eef81
Rory Hughes <rory.hughes.rory@gmail.com>
2019-02-26 13:23:27 +0000

    Makes chrome run in headless mode

--- tests/TEST_web.py
+++ tests/TEST_web.py
@@ -5,7 +5,9 @@ from selenium.webdriver.common.keys import Keys
 class TwitSearch(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
-
+        self.options =  self.driver.ChromeOptions()
+        self.options.add_argument('headless')
+        
     def test_search_in_python_org(self):
         driver = self.driver
         driver.get("localhost:5000")
