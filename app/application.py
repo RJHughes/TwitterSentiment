683c3aca73c846b4e2cbcc6ee6c1b3b6f2fab6d3
Rory Hughes <rory.hughes.rory@gmail.com>
2019-02-26 08:57:06 +0000

    Adds main handler

--- app/application.py
+++ app/application.py
@@ -15,40 +15,40 @@ import os
 application = Flask(__name__)
 application.config['SECRET_KEY'] = 'agile'
 
+if __name__ == '__main__':
+    @application.route('/', methods=['GET', 'POST'])
+    def search():
+        date_list = []
+        sentiment_list = []
+        hash_list = []
+        img_handle=[]
+        search=''
+        form = Search()
+        if request.method == 'POST':
+            search=request.form['search']
 
-@application.route('/', methods=['GET', 'POST'])
-def search():
-    date_list = []
-    sentiment_list = []
-    hash_list = []
-    img_handle=[]
-    search=''
-    form = Search()
-    if request.method == 'POST':
-        search=request.form['search']
-
-        # Error handling for invalid search case
-        if search == '':
-            search = ' '
-        if '-' in search[0]:
-            print('uh oh')
-            return render_template('search.html')
-
-
-        db= create_large_db('app/database/test.db')
+            # Error handling for invalid search case
+            if search == '':
+                search = ' '
+            if '-' in search[0]:
+                print('uh oh')
+                return render_template('search.html')
 
 
-        # This block performs a query on the text field of the db and looks
-        # for matches
-        start = time.time()
-        query = (db
-                 .select()
-                 .where(db.text.match(search)))
-        end = time.time()
-        print(end - start)
+            db= create_large_db('app/database/test.db')
 
-        date_list, sentiment_list, count_dict, hash_list = get_sentiment(query)
-        #hash_list = [['foo',120], ['bar',60]]
-        img_handle = create_wordcloud(hash_list)
 
-    return render_template('index.html', form=form, labels=date_list, values=sentiment_list, title=search, hash_list=json.dumps(hash_list),handle=img_handle)
+            # This block performs a query on the text field of the db and looks
+            # for matches
+            start = time.time()
+            query = (db
+                     .select()
+                     .where(db.text.match(search)))
+            end = time.time()
+            print(end - start)
+
+            date_list, sentiment_list, count_dict, hash_list = get_sentiment(query)
+            #hash_list = [['foo',120], ['bar',60]]
+            img_handle = create_wordcloud(hash_list)
+
+        return render_template('index.html', form=form, labels=date_list, values=sentiment_list, title=search, hash_list=json.dumps(hash_list),handle=img_handle)
