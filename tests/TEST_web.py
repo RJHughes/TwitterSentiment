f66819b85a2af42d5ca6c9e10ed502f187b6a111
Rory Hughes <rory.hughes.rory@gmail.com>
2019-02-26 12:24:07 +0000

    Adds selenium web testing

---
+++ tests/TEST_web.py
@@ -0,0 +1,22 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class TwitSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("localhost:5000")
+        print(driver.title)
+        #self.assertIn("Python", driver.title)
+        elem = driver.find_element_by_id('searchForm')
+        elem.send_keys("Twitter")
+        elem.send_keys(Keys.RETURN)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
