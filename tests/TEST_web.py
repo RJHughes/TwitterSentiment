d61f4e2bc5ac756da1fbd904a3d62f967ad974a6
HughesCoin <rory.hughes.rory@gmail.com>
2019-02-26 18:48:29 +0000

    Comments useless code

--- tests/TEST_web.py
+++ tests/TEST_web.py
@@ -6,16 +6,12 @@ from selenium.webdriver.chrome.options import Options
 class TwitSearch(unittest.TestCase):
     def setUp(self):
 
-        self.options = Options()
-        #self.options.add_argument('headless')
-        self.options.add_argument("--no-sandbox");
-        self.options.add_argument("--disable-dev-shm-usage");
-        self.driver = webdriver.Chrome(chrome_options=self.options)
+        self.driver = webdriver.Chrome(s)
 
     def test_search_in_python_org(self):
         driver = self.driver
         driver.get("localhost:5000")
-        print(driver.title)
+        #print(driver.title)
         #self.assertIn("Python", driver.title)
         elem = driver.find_element_by_id('searchForm')
         elem.send_keys("Twitter")
