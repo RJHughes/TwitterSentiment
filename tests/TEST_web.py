f36df3f4e4c8b7d2c3ff81b66ec8c6d804b4300c
Rory Hughes <rory.hughes.rory@gmail.com>
2019-02-26 13:33:56 +0000

    Last try to get web testing into CI

--- tests/TEST_web.py
+++ tests/TEST_web.py
@@ -1,13 +1,17 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 class TwitSearch(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.options =  self.driver.ChromeOptions()
-        self.options.add_argument('headless')
-        
+
+        self.options = Options()
+        #self.options.add_argument('headless')
+        self.options.add_argument("--no-sandbox");
+        self.options.add_argument("--disable-dev-shm-usage");
+        self.driver = webdriver.Chrome(chrome_options=self.options)
+
     def test_search_in_python_org(self):
         driver = self.driver
         driver.get("localhost:5000")
